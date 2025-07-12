35e29ea9b4d47be07cced082f5231212193067a3
Michał Dobranowski <mdbrnowski@gmail.com>
2025-07-12 12:16:26 +0200

    Implement input format translation

--- src/pabumeasures/main.py
+++ src/pabumeasures/main.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 
+from pabutools.election.ballot import Ballot, FrozenBallot
 from pabutools.election.instance import Instance, Project
 from pabutools.election.profile import Profile
 from pabutools.rules import BudgetAllocation
@@ -14,8 +15,33 @@ class Measure(Enum):
     ADD_SINGLETON = auto()
 
 
+def _translate_input_format(
+    instance: Instance, profile: Profile
+) -> tuple[list[Project], list[Ballot], int, list[int], list[list[int]]]:
+    if not isinstance(instance, Instance):
+        raise TypeError("Instance must be of type Instance")
+    if not isinstance(profile, Profile):
+        raise TypeError("Profile must be of type Profile")
+
+    projects: list[Project] = [project for project in instance]
+    _project_to_id = {project: i for i, project in enumerate(projects)}
+    ballots: list[Ballot] = [ballot for ballot in profile]
+    frozen_ballots: list[FrozenBallot] = [ballot.frozen() for ballot in profile]
+    _ballot_to_id = {ballot: i for i, ballot in enumerate(frozen_ballots)}
+    total_budget = int(instance.budget_limit)  # todo: remove int() if budget_limit can be float/mpq
+    cost: list[int] = [int(project.cost) for project in instance]  # todo: remove int() if cost can be float/mpq
+    approvers: list[list[int]] = [[] for _ in range(len(projects))]
+    for ballot in profile:
+        ballot_id = _ballot_to_id[ballot.frozen()]
+        for project in ballot:
+            approvers[_project_to_id[project]].append(ballot_id)
+    return projects, ballots, total_budget, cost, approvers
+
+
 def greedy(instance: Instance, profile: Profile) -> BudgetAllocation:
-    return BudgetAllocation(Project(str(i), 1) for i in _core.greedy())
+    projects, ballots, total_budget, cost, approvers = _translate_input_format(instance, profile)
+    result = _core.greedy(len(projects), len(ballots), total_budget, cost, approvers)
+    return BudgetAllocation(projects[i] for i in result)
 
 
 def greedy_measure(instance: Instance, profile: Profile, measure: Measure):
