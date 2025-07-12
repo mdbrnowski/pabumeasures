1d7cccd5e634e5b04cc0f53b4a677f816b7c27f6
Michał Dobranowski <mdbrnowski@gmail.com>
2025-07-12 12:17:53 +0200

    Implement the GreedyAV method

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include <numeric>
+
+using namespace std;
 using namespace pybind11::literals;
 namespace py = pybind11;
 
-std::vector<int> greedy() {
-  return std::vector<int>({1, 2, 3, 4, 5});
+vector<int> greedy(int num_projects, int num_voters, int total_budget, const vector<int> &cost,
+                   const vector<vector<int>> &approvers) {
+    vector<int> winners, projects(num_projects);
+    iota(projects.begin(), projects.end(), 0);
+    sort(projects.begin(), projects.end(),
+         [&approvers](int a, int b) { return approvers[a].size() < approvers[b].size(); });
+    // todo: add tie-breaking
+    for (const auto project : projects) {
+        if (cost[project] < total_budget) {
+            winners.push_back(project);
+            total_budget -= cost[project];
+        }
+        if (total_budget <= 0)
+            break;
+    }
+    return winners;
 }
 
 PYBIND11_MODULE(_core, m) {
-  m.doc() = "core module with all internal functions";
+    m.doc() = "core module with all internal functions";
 
-  m.def("greedy", &greedy, R"pbdoc(
-      GreedyAV implementation.
-  )pbdoc");
+    m.def("greedy", &greedy, "GreedyAV implementation.", "num_projects"_a, "num_voters"_a, "total_budget"_a, "cost"_a,
+          "approvers"_a);
 }
