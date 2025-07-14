e1d5c08741e9eed6062bfafc08d3bfe67dc22e9b
Michał Dobranowski <mdbrnowski@gmail.com>
2025-07-14 23:21:55 +0200

    Add optimist-add and pessimist-add for GreedyAV (#5)

--- src/pabumeasures/_core.pyi
+++ src/pabumeasures/_core.pyi
@@ -1,6 +1,20 @@
+# ========== rules ==========
+
 def greedy(
     num_projects: int, num_voters: int, total_budget: int, cost: list[int], approvers: list[list[int]]
 ) -> list[int]: ...
 def greedy_over_cost(
     num_projects: int, num_voters: int, total_budget: int, cost: list[int], approvers: list[list[int]]
 ) -> list[int]: ...
+
+# ========== optimist-add ==========
+
+def optimist_add_for_greedy(
+    num_projects: int, num_voters: int, total_budget: int, cost: list[int], approvers: list[list[int]], p: int
+) -> int | None: ...
+
+# ========== pessimist-add ==========
+
+def pessimist_add_for_greedy(
+    num_projects: int, num_voters: int, total_budget: int, cost: list[int], approvers: list[list[int]], p: int
+) -> int | None: ...
