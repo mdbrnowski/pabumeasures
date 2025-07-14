fddb5534c0e6c87cf13caeeb95c18b622496361e
Michał Dobranowski <mdbrnowski@gmail.com>
2025-07-15 00:36:12 +0200

    Add optimist-add and pessimist-add for GreedyAV/Cost (#6)

--- src/main.cpp
+++ src/main.cpp
@@ -34,14 +34,14 @@ optional<int> optimist_add_for_greedy(int num_projects, int num_voters, int tota
     sort(projects.begin(), projects.end(),
          [&approvers](int a, int b) { return approvers[a].size() > approvers[b].size(); });
     // todo: add tie-breaking
-    for (size_t i = 0; i < projects.size(); i++) {
+    for (int i = 0; i < num_projects; i++) {
         auto project = projects[i];
         if (cost[project] <= total_budget) {
             if (project == p)
                 return 0;
             if (total_budget - cost[project] < cost[p]) {
                 int difference = approvers[project].size() - approvers[p].size(); // todo: add tie-breaking
-                if (difference + approvers[p].size() > num_voters)
+                if (difference + static_cast<int>(approvers[p].size()) > num_voters)
                     return nullopt;
                 else
                     return difference;
@@ -78,6 +78,43 @@ vector<int> greedy_over_cost(int num_projects, int num_voters, int total_budget,
     return winners;
 }
 
+optional<int> optimist_add_for_greedy_over_cost(int num_projects, int num_voters, int total_budget,
+                                                const vector<int> &cost, const vector<vector<int>> &approvers, int p) {
+    if (cost[p] > total_budget)
+        return nullopt;
+    vector<int> projects(num_projects);
+    iota(projects.begin(), projects.end(), 0);
+    sort(projects.begin(), projects.end(), [&approvers, &cost](int a, int b) {
+        return approvers[a].size() / static_cast<double>(cost[a]) > approvers[b].size() / static_cast<double>(cost[b]);
+    });
+    // todo: add tie-breaking
+    for (int i = 0; i < num_projects; i++) {
+        auto project = projects[i];
+        if (cost[project] <= total_budget) {
+            if (project == p)
+                return 0;
+            if (total_budget - cost[project] < cost[p]) {
+                int difference = ceil(approvers[project].size() / static_cast<double>(cost[project]) *
+                                      static_cast<double>(cost[p])) -
+                                 approvers[p].size(); // todo: add tie-breaking
+                if (difference + static_cast<int>(approvers[p].size()) > num_voters)
+                    return nullopt;
+                else
+                    return difference;
+            }
+            total_budget -= cost[project];
+        }
+        if (total_budget <= 0)
+            break;
+    }
+    return nullopt;
+}
+
+optional<int> pessimist_add_for_greedy_over_cost(int num_projects, int num_voters, int total_budget,
+                                                 const vector<int> &cost, const vector<vector<int>> &approvers, int p) {
+    return optimist_add_for_greedy(num_projects, num_voters, total_budget, cost, approvers, p);
+}
+
 PYBIND11_MODULE(_core, m) {
     m.doc() = "core module with all internal functions";
 
@@ -90,6 +127,14 @@ PYBIND11_MODULE(_core, m) {
     m.def("pessimist_add_for_greedy", &pessimist_add_for_greedy, "pessimist-add measure for GreedyAV", "num_projects"_a,
           "num_voters"_a, "total_budget"_a, "cost"_a, "approvers"_a, "p"_a);
 
-    m.def("greedy_over_cost", &greedy_over_cost, "GreedyAV/Cost implementation/", "num_projects"_a, "num_voters"_a,
+    m.def("greedy_over_cost", &greedy_over_cost, "GreedyAV/Cost implementation", "num_projects"_a, "num_voters"_a,
           "total_budget"_a, "cost"_a, "approvers"_a);
+
+    m.def("optimist_add_for_greedy_over_cost", &optimist_add_for_greedy_over_cost,
+          "optimist-add measure for GreedyAV/Cost", "num_projects"_a, "num_voters"_a, "total_budget"_a, "cost"_a,
+          "approvers"_a, "p"_a);
+
+    m.def("pessimist_add_for_greedy_over_cost", &pessimist_add_for_greedy_over_cost,
+          "pessimist-add measure for GreedyAV/Cost", "num_projects"_a, "num_voters"_a, "total_budget"_a, "cost"_a,
+          "approvers"_a, "p"_a);
 }
--- tests/test_measures.py
+++ tests/test_measures.py
@@ -8,6 +8,10 @@ import pabumeasures
 
 NUMBER_OF_TIMES = 20
 
+# todo: test are not deterministic right now, because:
+# 1. random.choice(list(instance)) causes a problems
+# 2. pabutools does not use any seed or something like that
+
 
 @pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
 def test_greedy_measure(seed):
@@ -25,3 +29,23 @@ def test_greedy_measure(seed):
     else:
         # todo: it should be at least 1, but we need to allow 0 for now (lack of tie-breaking)
         assert result is not None and result >= 0
+
+
+@pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
+def test_greedy_over_cost_measure(seed):
+    random.seed(seed)
+    instance = get_random_instance(3, 1, 10)
+    profile = get_random_approval_profile(instance, 10)
+    allocation = pabumeasures.greedy_over_cost(instance, profile)
+    project = random.choice(list(instance))
+    result = pabumeasures.greedy_over_cost_measure(
+        instance, profile, project, pabumeasures.Measure.ADD_APPROVAL_OPTIMIST
+    )
+    print(instance, profile, allocation, project, result, sep="\n")
+    # todo: needs more extensive assertions
+    if project in allocation:
+        assert result == 0
+    elif project.cost > instance.budget_limit:
+        assert result is None
+    else:
+        assert result is None or result >= 0
