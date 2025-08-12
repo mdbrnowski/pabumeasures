195822f94171454932dc2eb3cfc34e4d6699f969
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-12 22:38:44 +0200

    Simplify test_comparator

--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,6 +1,5 @@
 import random
 import string
-from dataclasses import dataclass
 
 import pytest
 
@@ -16,32 +15,21 @@ def random_project(min_cost, max_cost, name_length=5, max_approvers=3):
 
 projects = [random_project(1, 3) for _ in range(200)]
 
-
-@dataclass(frozen=True)
-class ComparatorTestCase:
-    key_func: callable
-    reverse: bool
-    comparator: object
-    id: str
-
-
 test_cases = [
-    ComparatorTestCase(lambda p: p.cost, False, ProjectComparator.ByCostAsc, "ByCostAsc"),
-    ComparatorTestCase(lambda p: p.cost, False, ProjectComparator(COST, ASCENDING), "ByCostAsc_explicit"),
-    ComparatorTestCase(lambda p: p.cost, True, ProjectComparator(COST, DESCENDING), "ByCostDesc"),
-    ComparatorTestCase(lambda p: len(p.approvers), True, ProjectComparator.ByVotesDesc, "ByVotesDesc"),
-    ComparatorTestCase(lambda p: len(p.approvers), True, ProjectComparator(VOTES, DESCENDING), "ByVotesDesc_explicit"),
-    ComparatorTestCase(lambda p: len(p.approvers), False, ProjectComparator(VOTES, ASCENDING), "ByVotesAsc"),
-    ComparatorTestCase(
-        lambda p: (p.cost, -len(p.approvers)), False, ProjectComparator.ByCostAscThenVotesDesc, "ByCostAscThenVotesDesc"
-    ),
-    ComparatorTestCase(
+    (lambda p: p.cost, False, ProjectComparator.ByCostAsc, "ByCostAsc"),
+    (lambda p: p.cost, False, ProjectComparator(COST, ASCENDING), "ByCostAsc_explicit"),
+    (lambda p: p.cost, True, ProjectComparator(COST, DESCENDING), "ByCostDesc"),
+    (lambda p: len(p.approvers), True, ProjectComparator.ByVotesDesc, "ByVotesDesc"),
+    (lambda p: len(p.approvers), True, ProjectComparator(VOTES, DESCENDING), "ByVotesDesc_explicit"),
+    (lambda p: len(p.approvers), False, ProjectComparator(VOTES, ASCENDING), "ByVotesAsc"),
+    (lambda p: (p.cost, -len(p.approvers)), False, ProjectComparator.ByCostAscThenVotesDesc, "ByCostAscThenVotesDesc"),
+    (
         lambda p: (p.cost, -len(p.approvers)),
         False,
         ProjectComparator([(COST, ASCENDING), (VOTES, DESCENDING)]),
         "ByCostAscThenVotesDesc_explicit",
     ),
-    ComparatorTestCase(
+    (
         lambda p: (p.cost, -len(p.approvers), p.name),
         False,
         ProjectComparator([(COST, ASCENDING), (VOTES, DESCENDING), (LEXICOGRAPHIC, ASCENDING)]),
@@ -50,20 +38,20 @@ test_cases = [
 ]
 
 
-@pytest.mark.parametrize("case", test_cases, ids=lambda c: c.id)
-def test_project_comparator_basic(case: ComparatorTestCase):
+@pytest.mark.parametrize("key_func,reverse,comparator,name", test_cases)
+def test_project_comparator_basic(key_func, reverse, comparator, name):
     sorted_projects = list(projects)
-    sorted_projects.sort(key=case.key_func, reverse=case.reverse)
+    sorted_projects.sort(key=key_func, reverse=reverse)
 
     for i in range(len(sorted_projects)):
         for j in range(i + 1, len(sorted_projects)):
-            same_key = case.key_func(sorted_projects[i]) == case.key_func(sorted_projects[j])
+            same_key = key_func(sorted_projects[i]) == key_func(sorted_projects[j])
 
             if same_key:
-                assert not case.comparator(sorted_projects[i], sorted_projects[j])
-                assert not case.comparator(sorted_projects[j], sorted_projects[i])
+                assert not comparator(sorted_projects[i], sorted_projects[j])
+                assert not comparator(sorted_projects[j], sorted_projects[i])
             else:
-                assert case.comparator(sorted_projects[i], sorted_projects[j]) != case.comparator(
+                assert comparator(sorted_projects[i], sorted_projects[j]) != comparator(
                     sorted_projects[j], sorted_projects[i]
                 )
-                assert case.comparator(sorted_projects[i], sorted_projects[j])
+                assert comparator(sorted_projects[i], sorted_projects[j])
