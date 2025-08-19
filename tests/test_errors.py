40446e291f07702d65ec3b21ec2afbbad7159280
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-19 22:30:31 +0200

    Raise errors on some edge cases

---
+++ tests/test_errors.py
@@ -0,0 +1,41 @@
+import pytest
+from pabutools.election import ApprovalBallot, ApprovalProfile, Instance, Project
+
+import pabumeasures
+
+
+def test_error_on_same_names():
+    p1 = Project("p", 2)
+    p2 = Project("p", 1)
+    instance = Instance([p1, p2], 2)
+    if len(instance) != 2:
+        # pabutools removes duplicates
+        assert len(instance) == 1
+    else:
+        # if pabutools does not remove duplicates (for some reason), we should raise an error
+        profile = ApprovalProfile(
+            [
+                ApprovalBallot([p1]),
+                ApprovalBallot([p2]),
+                ApprovalBallot([p1, p2]),
+            ]
+        )
+
+        with pytest.raises(ValueError, match=r"names .+ unique"):
+            pabumeasures.greedy(instance, profile)
+
+
+def test_error_on_negative_cost():
+    p1 = Project("p1", -2)
+    p2 = Project("p2", 1)
+    instance = Instance([p1, p2], 2)
+    profile = ApprovalProfile(
+        [
+            ApprovalBallot([p1]),
+            ApprovalBallot([p2]),
+            ApprovalBallot([p1, p2]),
+        ]
+    )
+
+    with pytest.raises(ValueError, match=r"costs .+ positive"):
+        pabumeasures.greedy(instance, profile)
