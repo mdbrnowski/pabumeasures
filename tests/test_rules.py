fe44c6e86b1d8ca0ddc7317bac7fa56ed05f87da
Jakub Wisniewski <wisniewskij514@gmail.com>
2025-08-23 21:08:03 +0200

    Add tests to check user tie-breaking experience

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -4,7 +4,7 @@ import random
 import pytest
 from pabutools.election import Cardinality_Sat, Cost_Sat, parse_pabulib
 from pabutools.rules import greedy_utilitarian_welfare
-from pabutools.tiebreaking import min_cost_tie_breaking
+from pabutools.tiebreaking import max_cost_tie_breaking, min_cost_tie_breaking
 from utils import get_random_approval_profile, get_random_instance
 
 import pabumeasures
@@ -37,6 +37,23 @@ def test_greedy_random(seed):
     assert sorted(pabutools_result) == sorted(result)
 
 
+@pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
+def test_greedy_random_different_comparator(seed):
+    random.seed(seed)
+    instance, projects = get_random_instance(3, 1, 10)
+    profile = get_random_approval_profile(instance, projects, 10)
+    pabutools_result = greedy_utilitarian_welfare(
+        instance, profile, sat_class=Cost_Sat, tie_breaking=max_cost_tie_breaking
+    )
+    result = pabumeasures.greedy(
+        instance,
+        profile,
+        pabumeasures.ProjectComparator(pabumeasures.Comparator.COST, pabumeasures.Ordering.DESCENDING),
+    )
+
+    assert sorted(pabutools_result) == sorted(result)
+
+
 @pytest.mark.parametrize("file", test_files)
 def test_greedy_over_cost(file):
     instance, profile = parse_pabulib(file)
@@ -57,5 +74,22 @@ def test_greedy_over_cost_random(seed):
         instance, profile, sat_class=Cardinality_Sat, tie_breaking=min_cost_tie_breaking
     )
     result = pabumeasures.greedy_over_cost(instance, profile)
+
+    assert sorted(pabutools_result) == sorted(result)
+
+
+@pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
+def test_greedy_over_cost_random_different_comparator(seed):
+    random.seed(seed)
+    instance, projects = get_random_instance(3, 1, 10)
+    profile = get_random_approval_profile(instance, projects, 10)
+    pabutools_result = greedy_utilitarian_welfare(
+        instance, profile, sat_class=Cardinality_Sat, tie_breaking=max_cost_tie_breaking
+    )
+    result = pabumeasures.greedy_over_cost(
+        instance,
+        profile,
+        pabumeasures.ProjectComparator(pabumeasures.Comparator.COST, pabumeasures.Ordering.DESCENDING),
+    )
 
     assert sorted(pabutools_result) == sorted(result)
