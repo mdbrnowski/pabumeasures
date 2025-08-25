c8557715968cd5570033be5bf50df472f5e30a5e
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-25 20:59:03 +0200

    Remove `_translate_input_format_temp`

--- src/pabumeasures/main.py
+++ src/pabumeasures/main.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 
-from pabutools.election.ballot import Ballot, FrozenBallot
+from pabutools.election.ballot import FrozenBallot
 from pabutools.election.instance import Instance, Project
 from pabutools.election.profile import ApprovalProfile, Profile
 from pabutools.rules import BudgetAllocation
@@ -13,29 +13,6 @@ class Measure(Enum):
     ADD_APPROVAL_OPTIMIST = auto()
     ADD_APPROVAL_PESSIMIST = auto()
     ADD_SINGLETON = auto()
-
-
-def _translate_input_format_temp(
-    instance: Instance, profile: Profile
-) -> tuple[list[Project], list[Ballot], int, list[int], list[list[int]]]:
-    if not isinstance(instance, Instance):
-        raise TypeError("Instance must be of type Instance")
-    if not isinstance(profile, ApprovalProfile):
-        raise TypeError("Profile must be of type ApprovalProfile")
-
-    projects: list[Project] = sorted(instance)
-    _project_to_id = {project: i for i, project in enumerate(projects)}
-    ballots: list[Ballot] = [ballot for ballot in profile]
-    frozen_ballots: list[FrozenBallot] = [ballot.frozen() for ballot in profile]
-    _ballot_to_id = {ballot: i for i, ballot in enumerate(frozen_ballots)}
-    total_budget = int(instance.budget_limit)  # todo: remove int() if budget_limit can be float/mpq
-    cost: list[int] = [int(project.cost) for project in projects]  # todo: remove int() if cost can be float/mpq
-    approvers: list[list[int]] = [[] for _ in range(len(projects))]
-    for ballot in profile:
-        ballot_id = _ballot_to_id[ballot.frozen()]
-        for project in ballot:
-            approvers[_project_to_id[project]].append(ballot_id)
-    return projects, ballots, total_budget, cost, approvers
 
 
 def _translate_input_format(
