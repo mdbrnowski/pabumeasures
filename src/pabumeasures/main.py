dae88e010b1e470b466aaa379c2e886800b44cb5
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-26 10:12:11 +0200

    Add tie-breaking for greedyAV/Cost measures (#23)

--- src/pabumeasures/main.py
+++ src/pabumeasures/main.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 
-from pabutools.election.ballot import Ballot, FrozenBallot
+from pabutools.election.ballot import FrozenBallot
 from pabutools.election.instance import Instance, Project
 from pabutools.election.profile import ApprovalProfile, Profile
 from pabutools.rules import BudgetAllocation
@@ -13,29 +13,6 @@ class Measure(Enum):
     ADD_APPROVAL_OPTIMIST = auto()
     ADD_APPROVAL_PESSIMIST = auto()
     ADD_SINGLETON = auto()
-
-
-def _translate_input_format_temp(
-    instance: Instance, profile: Profile
-) -> tuple[list[Project], list[Ballot], int, list[int], list[list[int]]]:
-    if not isinstance(instance, Instance):
-        raise TypeError("Instance must be of type Instance")
-    if not isinstance(profile, ApprovalProfile):
-        raise TypeError("Profile must be of type ApprovalProfile")
-
-    projects: list[Project] = sorted(instance)
-    _project_to_id = {project: i for i, project in enumerate(projects)}
-    ballots: list[Ballot] = [ballot for ballot in profile]
-    frozen_ballots: list[FrozenBallot] = [ballot.frozen() for ballot in profile]
-    _ballot_to_id = {ballot: i for i, ballot in enumerate(frozen_ballots)}
-    total_budget = int(instance.budget_limit)  # todo: remove int() if budget_limit can be float/mpq
-    cost: list[int] = [int(project.cost) for project in projects]  # todo: remove int() if cost can be float/mpq
-    approvers: list[list[int]] = [[] for _ in range(len(projects))]
-    for ballot in profile:
-        ballot_id = _ballot_to_id[ballot.frozen()]
-        for project in ballot:
-            approvers[_project_to_id[project]].append(ballot_id)
-    return projects, ballots, total_budget, cost, approvers
 
 
 def _translate_input_format(
@@ -102,20 +79,18 @@ def greedy_over_cost(
     return BudgetAllocation(name_to_project[project_embeding.name] for project_embeding in result)
 
 
-def greedy_over_cost_measure(instance: Instance, profile: Profile, project: Project, measure: Measure) -> int | None:
-    projects, ballots, total_budget, cost, approvers = _translate_input_format_temp(instance, profile)
-    p = projects.index(project)
+def greedy_over_cost_measure(
+    instance: Instance, profile: Profile, project: Project, measure: Measure, tie_breaking=ProjectComparator.ByCostAsc
+) -> int | None:
+    total_budget, _, project_embeddings = _translate_input_format(instance, profile)
+    p = sorted(instance).index(project)
     match measure:
         case Measure.COST_REDUCTION:
             raise NotImplementedError()
         case Measure.ADD_APPROVAL_OPTIMIST:
-            return _core.optimist_add_for_greedy_over_cost(
-                len(projects), len(ballots), total_budget, cost, approvers, p
-            )
+            return _core.optimist_add_for_greedy_over_cost(total_budget, project_embeddings, p, tie_breaking)
         case Measure.ADD_APPROVAL_PESSIMIST:
-            return _core.pessimist_add_for_greedy_over_cost(
-                len(projects), len(ballots), total_budget, cost, approvers, p
-            )
+            return _core.pessimist_add_for_greedy_over_cost(total_budget, project_embeddings, p, tie_breaking)
         case Measure.ADD_SINGLETON:
             raise NotImplementedError()
 
