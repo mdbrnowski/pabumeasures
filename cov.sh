693c43db5085a774c2d2aee10e8eee5308b20286
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-27 22:49:54 +0200

    Create code coverage script

---
+++ cov.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+lcov --directory build --zerocounters
+CMAKE_ARGS="-DENABLE_COVERAGE=ON" uv pip install -e .
+echo "Running pytest"
+uv run pytest --maxfail=1 --disable-warnings --quiet
+lcov --capture --directory build \
+     --output-file coverage.info \
+     --filter brace \
+     --ignore-errors mismatch,mismatch,gcov,source,source
+lcov --remove coverage.info '/usr/*' '*/pybind11/*' '*/uv/python/*' \
+     -o coverage_filtered.info
+genhtml coverage_filtered.info --output-directory cpp_coverage_html
