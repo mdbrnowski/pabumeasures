4b31d17458b9c262d48957b761b3fca372227654
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-30 10:59:07 +0200

    Add singleton-add measure for GreedyAV/Cost

--- src/cpp_src/pb_methods_and_measures/GreedyOverCost.cpp
+++ src/cpp_src/pb_methods_and_measures/GreedyOverCost.cpp
@@ -82,3 +82,45 @@ std::optional<int> pessimist_add_for_greedy_over_cost(const Election &election, 
                                                       const ProjectComparator &tie_breaking) {
     return optimist_add_for_greedy_over_cost(election, p, tie_breaking);
 }
+
+std::optional<int> singleton_add_for_greedy_over_cost(const Election &election, int p,
+                                                      const ProjectComparator &tie_breaking) {
+    int total_budget = election.budget();
+    auto projects = election.projects();
+    auto pp = projects[p];
+    if (pp.cost() > total_budget)
+        return {}; // LCOV_EXCL_LINE (every project should be feasible)
+
+    std::vector<ProjectEmbedding> winners;
+    std::sort(projects.begin(), projects.end(), [&tie_breaking](ProjectEmbedding a, ProjectEmbedding b) {
+        long long cross_term_a_approvals_b_cost = static_cast<long long>(a.approvers().size()) * b.cost(),
+                  cross_term_b_approvals_a_cost = static_cast<long long>(b.approvers().size()) * a.cost();
+        if (cross_term_a_approvals_b_cost == cross_term_b_approvals_a_cost) {
+            return tie_breaking(a, b);
+        }
+        return cross_term_a_approvals_b_cost > cross_term_b_approvals_a_cost;
+    });
+    for (const auto &project : projects) {
+        if (project.cost() <= total_budget) {
+            if (project == pp) {
+                return 0;
+            }
+            if (pp.cost() > total_budget - project.cost()) { // if (last moment to add pp)
+                int new_approvers_size =
+                    pbmath::ceil_div(static_cast<long long>(project.approvers().size()) * pp.cost(), project.cost());
+                std::vector<int> new_approvers(new_approvers_size);
+                std::iota(new_approvers.begin(), new_approvers.end(), 0);
+                auto new_pp = ProjectEmbedding(pp.cost(), pp.name(), new_approvers);
+                if (static_cast<long long>(project.approvers().size()) * new_pp.cost() ==
+                        static_cast<long long>(new_pp.approvers().size()) * project.cost() &&
+                    tie_breaking(project, new_pp)) {
+                    new_approvers_size += 1;
+                }
+                return new_approvers_size - pp.approvers().size();
+            }
+            winners.push_back(project);
+            total_budget -= project.cost();
+        }
+    }
+    return {}; // LCOV_EXCL_LINE (every project should be feasible)
+}
