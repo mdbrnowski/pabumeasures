dfdf5eb5cebce95f5750cd020ba9bea459952699
Kuba Wisniewski <wisniewskij514@gmail.com>
2025-08-30 18:12:52 +0200

    Implement Mes-Cost

---
+++ src/cpp_src/pb_methods_and_measures/MesCost.cpp
@@ -0,0 +1,115 @@
+#include "MesCost.h"
+
+#include "utils/Election.h"
+#include "utils/Math.h"
+#include "utils/ProjectComparator.h"
+#include "utils/ProjectEmbedding.h"
+
+#include <algorithm>
+#include <limits>
+#include <queue>
+#include <ranges>
+#include <vector>
+
+std::vector<ProjectEmbedding> mes_cost(const Election &election, const ProjectComparator &tie_breaking) {
+    int total_budget = election.budget();
+    int n_voters = election.numVoters();
+    auto projects = election.projects();
+
+    std::vector<ProjectEmbedding> winners;
+
+    struct Candidate {
+        int index, num_approvals;
+        long double max_payment_by_cost;
+    };
+
+    auto pq_cmp = [](const Candidate &a, const Candidate &b) {
+        if (pbmath::is_equal(a.max_payment_by_cost, b.max_payment_by_cost))
+            return a.num_approvals < b.num_approvals;
+        return a.max_payment_by_cost > b.max_payment_by_cost;
+    };
+
+    std::priority_queue<Candidate, std::vector<Candidate>, decltype(pq_cmp)> remaining_candidates(pq_cmp);
+
+    for (int i = 0; i < projects.size(); i++) {
+        remaining_candidates.emplace(i, projects[i].approvers().size(), 0);
+    }
+
+    std::vector<long double> budget(n_voters, static_cast<long double>(total_budget) / n_voters);
+
+    std::vector<Candidate> candidates_to_reinsert;
+    candidates_to_reinsert.reserve(projects.size());
+
+    while (true) {
+        long double min_max_payment_by_cost = std::numeric_limits<long double>::max();
+        Candidate best_candidate;
+
+        while (!remaining_candidates.empty()) {
+            auto current_candidate = remaining_candidates.top();
+            remaining_candidates.pop();
+            auto project = projects[current_candidate.index];
+            long double previous_max_payment_by_cost = current_candidate.max_payment_by_cost;
+
+            if (pbmath::is_greater_than(previous_max_payment_by_cost, min_max_payment_by_cost)) {
+                candidates_to_reinsert.push_back(current_candidate);
+                break; // We already selected the best possible - max_payment value can only increase
+            }
+
+            long double money_behind_project = 0;
+            auto approvers = project.approvers();
+
+            for (const auto &approver : approvers) {
+                money_behind_project += budget[approver];
+            }
+
+            if (pbmath::is_less_than(money_behind_project, project.cost())) {
+                continue;
+            }
+
+            std::ranges::sort(approvers, [&budget](const int a, const int b) { return budget[a] < budget[b]; });
+
+            long double paid_so_far = 0, denominator = approvers.size();
+
+            for (const auto &approver : approvers) {
+                long double max_payment = (static_cast<long double>(project.cost()) - paid_so_far) / denominator;
+                long double max_payment_by_cost = max_payment / project.cost();
+                if (pbmath::is_greater_than(max_payment, budget[approver])) { // cannot afford to fully participate
+                    paid_so_far += budget[approver];
+                    denominator--;
+                } else { // from this voter, everyone can fully participate
+                    current_candidate.max_payment_by_cost = max_payment_by_cost;
+                    if (pbmath::is_less_than(max_payment_by_cost, min_max_payment_by_cost) ||
+                        (pbmath::is_equal(max_payment_by_cost, min_max_payment_by_cost) &&
+                         tie_breaking(project, projects[best_candidate.index]))) {
+                        if (min_max_payment_by_cost !=
+                            std::numeric_limits<long double>::max()) { // Not the first "best" candidate
+                            candidates_to_reinsert.push_back(best_candidate);
+                        }
+                        min_max_payment_by_cost = max_payment_by_cost;
+                        best_candidate = current_candidate;
+                    } else {
+                        candidates_to_reinsert.push_back(current_candidate);
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (min_max_payment_by_cost == std::numeric_limits<long double>::max()) { // No more affordable projects
+            break;
+        }
+        winners.push_back(projects[best_candidate.index]);
+
+        for (const auto &approver : projects[best_candidate.index].approvers()) {
+            budget[approver] =
+                std::max(0.0L, budget[approver] - min_max_payment_by_cost * projects[best_candidate.index].cost());
+        }
+
+        for (auto &candidate : candidates_to_reinsert) {
+            remaining_candidates.push(std::move(candidate));
+        }
+        candidates_to_reinsert.clear();
+    }
+
+    return winners;
+}
