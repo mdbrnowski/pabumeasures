53b2be52babc04d8e398f9e3e2ec49b35240267a
Jakub Wiśniewski <119816982+wisniewskij@users.noreply.github.com>
2025-09-02 11:56:28 +0200

    Fix MES code coverage problem (put `Candidate` struct in an anonymous namespace) (#40)

--- cov.sh
+++ cov.sh
@@ -6,7 +6,9 @@ uv run pytest --maxfail=1 --disable-warnings --quiet
 lcov --capture --directory build \
      --output-file coverage.info \
      --filter brace \
-     --ignore-errors mismatch,mismatch,gcov,source,source
+     --ignore-errors mismatch,mismatch,gcov,source,source,inconsistent,unsupported
 lcov --remove coverage.info '/usr/*' '*/pybind11/*' '*/uv/python/*' \
-     -o coverage_filtered.info
-genhtml coverage_filtered.info --output-directory cpp_coverage_html
+     -o coverage_filtered.info --ignore-errors unused
+
+genhtml coverage_filtered.info --output-directory cpp_coverage_html --ignore-errors category
+
