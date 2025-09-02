a9b3486e9f34eb329dbdef3a4fc41acb728184d6
Michał Dobranowski <mdbrnowski@gmail.com>
2025-09-02 22:25:38 +0200

    Implement cost-red measure for Phragmen

--- src/cpp_src/pb_methods_and_measures/Phragmen.cpp
+++ src/cpp_src/pb_methods_and_measures/Phragmen.cpp
@@ -54,3 +54,79 @@ std::vector<ProjectEmbedding> phragmen(const Election &election, const ProjectCo
     }
     return winners;
 }
+
+std::optional<long long> cost_reduction_for_phragmen(const Election &election, int p,
+                                                     const ProjectComparator &tie_breaking) {
+    auto total_budget = election.budget();
+    auto n_voters = election.numVoters();
+    auto projects = election.projects();
+    auto pp = projects[p];
+    std::vector<long double> load(n_voters, 0);
+
+    if (pp.approvers().empty()) {
+        return 0;
+    }
+
+    std::optional<long long> max_price_to_be_chosen{};
+
+    while (!projects.empty()) {
+        long double min_max_load = std::numeric_limits<long double>::max();
+        std::vector<ProjectEmbedding> round_winners;
+        for (const auto &project : projects) {
+            long double max_load = project.cost();
+            if (project.approvers().empty()) {
+                max_load = std::numeric_limits<long double>::max();
+            } else {
+                for (const auto &approver : project.approvers())
+                    max_load += load[approver];
+                max_load /= project.approvers().size();
+            }
+
+            if (pbmath::is_less_than(max_load, min_max_load)) {
+                round_winners.clear();
+                min_max_load = max_load;
+            }
+            if (pbmath::is_equal(max_load, min_max_load)) {
+                round_winners.push_back(project);
+            }
+        }
+
+        bool normally_would_break =
+            min_max_load == std::numeric_limits<long double>::max() ||
+            any_of(round_winners.begin(), round_winners.end(),
+                   [total_budget](const ProjectEmbedding &winner) { return winner.cost() > total_budget; });
+
+        auto winner = *std::ranges::min_element(round_winners, tie_breaking);
+
+        if (winner == pp && !normally_would_break) {
+            return pp.cost();
+        }
+
+        long double load_sum = 0;
+        for (const auto &approver : pp.approvers()) {
+            load_sum += load[approver];
+        }
+        long long curr_max_price = pbmath::floor(min_max_load * pp.approvers().size() - load_sum);
+        curr_max_price = std::min({curr_max_price, pp.cost(), total_budget});
+
+        long double pp_max_load = (curr_max_price + load_sum) / pp.approvers().size();
+        if (pbmath::is_equal(pp_max_load, min_max_load) &&
+            (normally_would_break ||
+             tie_breaking(winner, ProjectEmbedding(curr_max_price, pp.name(), pp.approvers())))) {
+            curr_max_price--;
+        }
+        max_price_to_be_chosen = pbmath::optional_max(max_price_to_be_chosen, curr_max_price);
+
+        if (normally_would_break) {
+            break;
+        }
+
+        for (const auto &approver : winner.approvers()) {
+            load[approver] = min_max_load;
+        }
+
+        total_budget -= winner.cost();
+        projects.erase(remove(projects.begin(), projects.end(), winner), projects.end());
+    }
+    return max_price_to_be_chosen;
+}
