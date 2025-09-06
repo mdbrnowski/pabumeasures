08923a229a032efd78557d264bec53fc09c71053
Michał Dobranowski <mdbrnowski@gmail.com>
2025-09-06 13:58:11 +0200

    Reformat

--- src/cpp_src/pb_methods_and_measures/Phragmen.h
+++ src/cpp_src/pb_methods_and_measures/Phragmen.h
@@ -7,4 +7,5 @@
 
 std::vector<ProjectEmbedding> phragmen(const Election &election, const ProjectComparator &tie_breaking);
 
-std::optional<long long> cost_reduction_for_phragmen(const Election &election, int p, const ProjectComparator &tie_breaking);
+std::optional<long long> cost_reduction_for_phragmen(const Election &election, int p,
+                                                     const ProjectComparator &tie_breaking);
