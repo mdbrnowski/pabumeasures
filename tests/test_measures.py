7700d990d42d003cc8e0ca79a362bcd28cf4c49b
Michał Dobranowski <mdbrnowski@gmail.com>
2025-09-06 15:38:17 +0200

    Parametrize singleton-add measure

--- tests/test_measures.py
+++ tests/test_measures.py
@@ -36,25 +36,6 @@ def test_greedy_measure(seed, measure):
 
 
 @pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
-def test_singleton_add_for_greedy(seed):
-    random.seed(seed)
-    instance, profile = get_random_election()
-    project = get_random_project(instance)
-    allocation = pabumeasures.greedy(instance, profile)
-    result = pabumeasures.greedy_measure(instance, profile, project, Measure.ADD_SINGLETON)
-    assert result is not None
-    if project in allocation:
-        assert result == 0
-    else:
-        assert result >= 1
-        for i in range(len(profile), len(profile) + result):
-            profile.append(ApprovalBallot({project}, name=f"SingletonAppBallot {i}"))
-        assert project in pabumeasures.greedy(instance, profile)
-        profile[-1].remove(project)
-        assert project not in pabumeasures.greedy(instance, profile)
-
-
-@pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
 @pytest.mark.parametrize("measure", [Measure.ADD_APPROVAL_OPTIMIST, Measure.ADD_APPROVAL_PESSIMIST])
 def test_greedy_over_cost_measure(seed, measure):
     random.seed(seed)
@@ -80,12 +61,20 @@ def test_greedy_over_cost_measure(seed, measure):
 
 
 @pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
-def test_singleton_add_for_greedy_over_cost(seed):
+@pytest.mark.parametrize(
+    "rule,rule_measure",
+    [
+        (pabumeasures.greedy, pabumeasures.greedy_measure),
+        (pabumeasures.greedy_over_cost, pabumeasures.greedy_over_cost_measure),
+    ],
+    ids=["greedy", "greedy_over_cost"],
+)
+def test_singleton_add_measure(seed, rule, rule_measure):
     random.seed(seed)
     instance, profile = get_random_election()
     project = get_random_project(instance)
-    allocation = pabumeasures.greedy_over_cost(instance, profile)
-    result = pabumeasures.greedy_over_cost_measure(instance, profile, project, Measure.ADD_SINGLETON)
+    allocation = rule(instance, profile)
+    result = rule_measure(instance, profile, project, Measure.ADD_SINGLETON)
     assert result is not None
     if project in allocation:
         assert result == 0
@@ -93,9 +82,9 @@ def test_singleton_add_for_greedy_over_cost(seed):
         assert result >= 1
         for i in range(len(profile), len(profile) + result):
             profile.append(ApprovalBallot({project}, name=f"SingletonAppBallot {i}"))
-        assert project in pabumeasures.greedy_over_cost(instance, profile)
+        assert project in rule(instance, profile)
         profile[-1].remove(project)
-        assert project not in pabumeasures.greedy_over_cost(instance, profile)
+        assert project not in rule(instance, profile)
 
 
 @pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
