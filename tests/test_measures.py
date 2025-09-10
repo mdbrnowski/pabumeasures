5abf40649d7acea884443a1e15abfa6d1af66093
Michał Dobranowski <mdbrnowski@gmail.com>
2025-09-10 19:59:30 +0200

    Standardize the parametrization of existing tests

--- tests/test_measures.py
+++ tests/test_measures.py
@@ -11,13 +11,22 @@ NUMBER_OF_TIMES = 500
 
 
 @pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
-@pytest.mark.parametrize("measure", [Measure.ADD_APPROVAL_OPTIMIST, Measure.ADD_APPROVAL_PESSIMIST])
-def test_greedy_measure(seed, measure):
+@pytest.mark.parametrize(
+    "rule,rule_measure",
+    [
+        (pabumeasures.greedy, pabumeasures.greedy_measure),
+        (pabumeasures.greedy_over_cost, pabumeasures.greedy_over_cost_measure),
+    ],
+    ids=["greedy", "greedy_over_cost"],
+)
+def test_optimist_add_and_pessimist_add_measure(seed, rule, rule_measure):
+    """Test optimist-add and pessimist-add measures for rules for which they are equal."""
     random.seed(seed)
     instance, profile = get_random_election()
     project = get_random_project(instance)
-    allocation = pabumeasures.greedy(instance, profile)
-    result = pabumeasures.greedy_measure(instance, profile, project, measure)
+    allocation = rule(instance, profile)
+    result = rule_measure(instance, profile, project, Measure.ADD_APPROVAL_OPTIMIST)
+    assert result == rule_measure(instance, profile, project, Measure.ADD_APPROVAL_PESSIMIST)
     if project in allocation:
         assert result == 0
     else:
@@ -25,14 +34,14 @@ def test_greedy_measure(seed, measure):
         if result is None:
             for na in non_approvers:
                 na.add(project)
-            assert project not in pabumeasures.greedy(instance, profile)
+            assert project not in rule(instance, profile)
         else:
             assert 1 <= result <= len(non_approvers)
             for na in non_approvers[:result]:
                 na.add(project)
-            assert project in pabumeasures.greedy(instance, profile)
+            assert project in rule(instance, profile)
             non_approvers[0].remove(project)
-            assert project not in pabumeasures.greedy(instance, profile)
+            assert project not in rule(instance, profile)
 
 
 @pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
