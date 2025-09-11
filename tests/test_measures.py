6c079b515baecbf8edf8686dba3ac42b74a3ea0d
Kuba Wisniewski <wisniewskij514@gmail.com>
2025-09-11 23:46:54 +0200

    Merge singleton-add tests

--- tests/test_measures.py
+++ tests/test_measures.py
@@ -66,9 +66,11 @@ def test_greedy_over_cost_measure(seed, measure):
     [
         (pabumeasures.greedy, pabumeasures.greedy_measure),
         (pabumeasures.greedy_over_cost, pabumeasures.greedy_over_cost_measure),
+        (pabumeasures.mes_apr, pabumeasures.mes_apr_measure),
+        (pabumeasures.mes_cost, pabumeasures.mes_cost_measure),
         (pabumeasures.phragmen, pabumeasures.phragmen_measure),
     ],
-    ids=["greedy", "greedy_over_cost", "phragmen"],
+    ids=["greedy", "greedy_over_cost", "mes_apr", "mes_cost", "phragmen"],
 )
 def test_singleton_add_measure(seed, rule, rule_measure):
     random.seed(seed)
@@ -77,35 +79,8 @@ def test_singleton_add_measure(seed, rule, rule_measure):
     allocation = rule(instance, profile)
     result = rule_measure(instance, profile, project, Measure.ADD_SINGLETON)
 
-    assert result is not None
-    if project in allocation:
-        assert result == 0
-    else:
-        assert result >= 1
-        for i in range(len(profile), len(profile) + result):
-            profile.append(ApprovalBallot({project}, name=f"SingletonAppBallot {i}"))
-        assert project in rule(instance, profile)
-        profile.pop()
-        assert project not in rule(instance, profile)
-
-
-@pytest.mark.parametrize("seed", list(range(NUMBER_OF_TIMES)))
-@pytest.mark.parametrize(
-    "rule,rule_measure",
-    [
-        (pabumeasures.mes_apr, pabumeasures.mes_apr_measure),
-        (pabumeasures.mes_cost, pabumeasures.mes_cost_measure),
-    ],
-    ids=["mes_apr", "mes_cost"],
-)
-def test_singleton_add_measure_for_mes(seed, rule, rule_measure):
-    random.seed(seed)
-    instance, profile = get_random_election()
-    project = get_random_project(instance)
-    allocation = rule(instance, profile)
-    result = rule_measure(instance, profile, project, Measure.ADD_SINGLETON)
-
     if result is None:
+        assert rule in [pabumeasures.mes_apr, pabumeasures.mes_cost]
         assert instance.budget_limit == project.cost
     else:
         if project in allocation:
